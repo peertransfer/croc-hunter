e74bc3883b3701c34d09ee1d961714fdfab7a60a
Jose Luis Salas <josacar@users.noreply.github.com>
2017-03-15 17:29:47 +0000

    add create_croc-hunter-controller.json.sh

---
+++ create_croc-hunter-controller.json.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat > croc-hunter-controller.json <<EOF
+{
+  "kind": "ReplicationController",
+  "apiVersion": "v1beta3",
+  "metadata": {
+    "name": "croc-hunter",
+    "labels": {
+      "name": "croc-hunter"
+    }
+  },
+  "spec": {
+    "replicas": 3,
+    "selector": {
+      "name": "croc-hunter"
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "name": "croc-hunter",
+          "deployment": "${WERCKER_GIT_COMMIT}"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "imagePullPolicy": "Always",
+            "image": "quay.io/flywire/croc-hunter:${WERCKER_GIT_COMMIT}",
+            "name": "croc-hunter",
+            "ports": [
+              {
+                "name": "http-server",
+                "containerPort": 5000,
+                "protocol": "TCP"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
